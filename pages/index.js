7893015031ca6563ba36bf5dd0a3d60989364db6
pungprakearti <andrew.pungprakearti@gmail.com>
2022-11-16 12:09:57 -0800

    Add title to index page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
-import PatternLibrary from '@/components/PatternLibrary'
-import cx from 'classnames'
-import styles from './index.module.scss'
+import PatternLibrary from "@/components/PatternLibrary"
+import cx from "classnames"
+import Head from "next/head"
+import styles from "./index.module.scss"
 
 export default function Home() {
-
   return (
-    <div className={cx(styles.wrap, ' container')}>
+    <div className={cx(styles.wrap, " container")}>
+      <Head>
+        <title>Lighthouse test</title>
+      </Head>
       <span>
-        <h2>
-          NextJS Boilerplate by
-        </h2>
-        <p> <a href='mailto:andrew.pungprakearti@gmail.com'>Andrew Pungprakearti</a></p>
+        <h2>NextJS Boilerplate by</h2>
+        <p>
+          {" "}
+          <a href='mailto:andrew.pungprakearti@gmail.com'>
+            Andrew Pungprakearti
+          </a>
+        </p>
       </span>
       <PatternLibrary />
     </div>
